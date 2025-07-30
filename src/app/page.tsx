daef56ef2d5b1e2e1958d71a609434340bc3b7c5
H M Merajul Hasan <94540374+HNMerajulhasan@users.noreply.github.com>
2025-07-31 00:10:55 +0600

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import right_hero_image from "../../public/assest/hero_right_image.svg";
 import fame_tonic_logo from "../../public/assest/fame-tonic-logo.svg";
 import note_star_icon from "../../public/assest/note-star-icon.png";
 import hero_button from "../../public/assest/hero_button.svg";
+import burger_menu from "../../public/assest/burger_menu.svg";
+import rocket from "../../public/assest/rocket-removebg.png";
+
 // import { Star, ArrowRight } from 'lucide-react';
 
 export default function Home() {
@@ -13,16 +16,28 @@ export default function Home() {
       <div className="w-full bg-gradient-to-r from-[#E2185F] via-purple-500 to-cyan-400 md:px-[279px]">
         <div className="md:max-w-[1440px] mx-auto  md:flex items-center justify-center md:h-[46px] h-[59px]">
           {/* <Star className="w-4 h-4 text-yellow-300 mr-2" /> */}
-          <span className="md:text-[22px] text-[14px] text-white md:flex">
+          <span className="md:text-[22px] text-[14px] text-white md:flex hidden">
             <strong className="text-[#00E7F9] font-extrabold font-figtree">FRESH BEGINNINGS SALE: </strong>  {" "} <span className="font-figtree font-bold ml-1">Extra 25% OFF, Limited Spots
             - start your journey today!</span>
           </span>
+          {/* mobile text */}
+           <div className=" text-white md:hidden flex justify-center items-center">
+            <h1 className="text-[#00E7F9] text-[16px] font-extrabold font-figtree">
+              <span>
+                <Image
+                  src={rocket}
+                  alt="Rocket Icon"
+                  className="inline-block w-[20px] h-[20px] mr-2"
+                />
+              </span>
+              FRESH BEGINNINGS SALE: <span className="text-[#FFFFFF] ml-1">!</span></h1> 
+          </div>
         </div>
       </div>
 
       <div className="">
         {/* Background Image Positioned to the Right */}
-        <div className="absolute md:right-[99px] md:top-[126px] z-0  pointer-events-none md:pt-[9px] pt-[40px] md:px-0 px-10">
+        <div className="absolute md:right-[99px] md:top-[126px] z-0  pointer-events-none md:pt-[9px] pt-[15px] md:px-0 px-10">
           <Image
             src={right_hero_image}
             alt="Hero Right Background"
@@ -31,13 +46,26 @@ export default function Home() {
         </div>
 
         {/* Mobile Logo */}
-          <div className="md:hidden flex justify-center mt-[33px]">
+        <div>
+            <div className="md:hidden flex justify-center mt-[33px]">
               <Image
                 src={fame_tonic_logo}
                 alt="Fame Tonic Logo"
                 className="w-[107.78px] h-[46.07px]"
               />
+              
             </div>
+            <div>
+              <div className="md:hidden flex justify-end -translate-y-[150%] mr-[29.22px]">
+                <Image
+                  src={burger_menu}
+                  alt="Burger Menu Icon"
+                  className="w-[24px] h-[24px]"
+                />
+              </div>
+            </div>
+        </div>
+        
 
         {/* Main Content */}
         <div className="relative z-10 max-w-[1440px] mx-auto ">
@@ -105,21 +133,21 @@ export default function Home() {
 
               {/* Mobile Footer Text */}
               <div className="mt-[22px] md:hidden block">
-                <p className="text-[#ABABAB] text-[12px] font-medium font-figtree">
+                <p className="text-[#ABABAB] text-[12px] font-medium font-figtree text-center px-[54.5px]">
                   By clicking "Get Started", you agree with Terms and
-                  Conditions, Privacy Policy, Subscription<br/> Terms
+                  Conditions, Privacy Policy, Subscription Terms
                 </p>
-                <p className="text-[10px] font-medium text-[#ABABAB] mt-[12px] font-figtree">Fametonic 2025 ©All Rights Reserved.</p>
+                <p className="text-[10px] text-center font-medium text-[#ABABAB] mt-[19px] font-figtree">Fametonic 2025 ©All Rights Reserved.</p>
               </div>
 
 
               {/* CTA Button */}
               <div className="mt-[25px]">
                 <button className="cursor-pointer">
-                  <Image src={hero_button} alt="alt"  className=""/>
+                  <Image src={hero_button} alt="alt"  className="md:w-[313px] w-[350px] h-[60px]"/>
                   {/* <ArrowRight className="w-5 h-5" /> */}
                 </button>
-                <p className="text-sm text-[#FFFFFF] ml-[9.5%] -translate-y-2 text-[12px] font-figtree">
+                <p className="text-sm text-[#FFFFFF] md:ml-[9.5%] md:text-start text-center -translate-y-2 text-[12px] font-figtree">
                   1-minute quiz for personalized insights
                 </p>
               </div>
